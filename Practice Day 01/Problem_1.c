b95b508e6cd820b89285db3e34a21dbd78bcb71e
irfanekon04 <irfanekon2004@gmail.com>
2024-04-05 02:56:05 +0600

    fixed some formatting

--- Practice Day 01/Problem_1.c
+++ Practice Day 01/Problem_1.c
@@ -2,6 +2,6 @@
 
 int  main()
 {
-    printf("Recently I heard that you've achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you'll shine in your life!  Good luck with all the barriers(/\\) in your life.");
+    printf("Recently I heard that you've achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you\'ll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
     return 0;
 }
