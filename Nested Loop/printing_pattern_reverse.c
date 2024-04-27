8e18b132f82a22b2ecf4f229366d0ee0c267fb47
irfanekon04 <irfanekon2004@gmail.com>
2024-04-28 02:13:23 +0600

    nested loop practice

---
+++ Nested Loop/printing_pattern_reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int k=n;
+    for (int i=0;i<n;i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        k--;
+    }
+    return 0;
+}
