5a8b277b0f9305cd59b5feb0032700e28193fce6
irfanekon04 <irfanekon2004@gmail.com>
2024-05-19 03:01:25 +0600

    cpp

---
+++ C++ for DS/1 Basic C++/output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"hello c++"<<std::endl; //str
+    int a=10;
+    std::cout<<a<<std::endl;
+    std::cout<<"the number is "<< a<<std::endl;
+    std::cout<<a<<" is my favourite number."<<std::endl;
+    std::cout<<"my favourite number is "<<a<<" TEN"<<std::endl;
+    return 0;
+}
