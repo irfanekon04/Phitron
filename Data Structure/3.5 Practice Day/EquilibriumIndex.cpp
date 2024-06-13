d4e76cdc2af9e42c1a4796a342adec8a393cceac
irfanekon04 <irfanekon2004@gmail.com>
2024-06-13 16:19:39 +0600

    new

--- Data Structure/3.5 Practice Day/EquilibriumIndex.cpp
+++ Data Structure/3.5 Practice Day/EquilibriumIndex.cpp
@@ -9,6 +9,20 @@ int main()
     {
         cin >> v[i];
     }
-
+    int totalSum = 0, leftSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        totalSum += v[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int rightSum = totalSum - leftSum - v[i];
+        if (leftSum == rightSum)
+        {
+            cout << i << endl;
+            break;
+        }
+        leftSum += v[i];
+    }
     return 0;
 }
