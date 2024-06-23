b47cbd384419e002cea96e25fb6d8d82f7e01473
irfanekon04 <irfanekon2004@gmail.com>
2024-06-24 03:15:23 +0600

    module 5

---
+++ Data Structure/5 Singly Linked List/dynamicNode.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+
+    head->next = a;
+
+    cout << head->val << endl;
+    cout << head->next->val << endl;
+    cout << a->val << endl;
+    return 0;
+}
